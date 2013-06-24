4edab80fa27b6bd92552420780b7a5f2b2fb3106
Matthew Bailey <mematt8985@gmail.com>
2013-06-24 10:54:51 -0700

    added create and update CRUD actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,29 @@ class PostsController < ApplicationController
   def new
   end
 
+  def create
+  	@post = Post.new(params[:post])
+  	raise
+  		if @post.save
+  		flash[:notice] = "Post was saved"
+  		redirect_to @post
+  		else
+  		flas[:error] = "There was an error saving the post. Please try again."
+  		render :new
+  		end
+    end
+
   def edit
+    @post = Post.find(params[:id])
   end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(params[:post])
+      flash[:notice] = "Post was updated."
+      redirect_to @post
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :new
+    end
 end
