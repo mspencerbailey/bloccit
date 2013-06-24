ac06dc7c390eea9a996772db939a3fc511145546
Matthew Bailey <mematt8985@gmail.com>
2013-06-24 08:12:59 -0700

    CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,10 @@
 class PostsController < ApplicationController
   def index
+  	@posts = post.all
   end
 
   def show
+  	@post / Post.find(params[:id])
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Bloccit::Application.routes.draw do
-  resources :posys
+  resources :posts
 
   match "about"  => 'welcome#about', via: :get
 
